055805c149f570f09a0862f7931871fab735a1ae
GloomyEntity <kirillgolovkin13@gmail.com>
2025-03-30 02:00:42 +0300

    Create Narik.h

---
+++ Narik/Narik.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Smoke {
+public:
+	int Nar_x = 1; int Nar_y = 2;
+	int Nar_MapNumber = 2;
+	string NaricNPC = " Г ";
+	bool Nar_speak = true;
+};
+Smoke NAR;
