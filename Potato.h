4b410fd3a0244c406921ab7c9cb225c133030523
GloomyEntity <kirillgolovkin13@gmail.com>
2025-03-30 02:04:53 +0300

    Create Potato.h

---
+++ Potato.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mystical_stone {
+public:
+    string loot_item = " * ";
+    int li_y = 4, li_x = 4;
+    string mystical_stone = "mystical stone";
+    bool pick_loot_item = false;
+};
+Mystical_stone stone;
